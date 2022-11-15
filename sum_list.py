518b07eee217e76e977cc0ebbfdc584c6184d6ea
vzacchigna <115220951+vzacchigna@users.noreply.github.com>
2022-11-15 16:44:40 +0100

    Update sum_list.py

--- sum_list.py
+++ sum_list.py
@@ -1,11 +1,11 @@
-def sum_(list):
+def sum_list(list):
     somma=0
     if list==[]:
-        return none
+        return None
     for item in list:
         somma=somma+item
     return somma
     
-list=[1,2,3,4]
-somma=sum(list)
+list=[]
+somma=sum_list(list)
 print(somma)
