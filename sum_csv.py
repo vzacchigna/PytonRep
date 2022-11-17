98a16b66416fea39e98343575843f6518cb75f23
vzacchigna <115220951+vzacchigna@users.noreply.github.com>
2022-11-17 17:50:30 +0100

    Create sum_csv.py

---
+++ sum_csv.py
@@ -0,0 +1,19 @@
+def sum_csv(my_file):
+    list=[]
+    my_file=open('shampoo_sales.csv','r')
+    def sum_list(list):
+        somma=0
+        if list==[]:
+            return None
+        for item in list:
+            somma=somma+item
+        return somma
+    for line in my_file:
+        elements=line.split(',')
+        numbers=float(elements)
+        date=elements[0]
+        value=elements[1]
+        list.append(float(value))
+    s=sum_list(list)
+    return s
+print(sum_csv(my_file))
