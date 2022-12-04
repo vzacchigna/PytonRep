ee8f6617989b92601754bfdc87f061b3c66cf560
vzacchigna <115220951+vzacchigna@users.noreply.github.com>
2022-12-04 17:01:06 +0100

    Create es4a.py

---
+++ es4a.py
@@ -0,0 +1,21 @@
+class CSVFile:
+    
+    def __init__ (self, file_name):
+        self.name = file_name
+        self.can_read = True
+    def get_data(self):
+        file_reader = open(,'r')
+        
+        if file_reader.readline():
+            print("file presente")
+        else:
+            print("Errore")
+        list=[]
+        
+        for line in file_reader:
+            elements = line.split(',')
+            list.append(elements)
+        
+        return list
+csv_file = CSVFile()
+list = csv_file.get_data()
